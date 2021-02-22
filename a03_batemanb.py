d1d270858199c51bb13df69dfd2f97fd9ee51e82
batemanb <batemanb@berea.edu>
2021-02-21 20:28:17 -0500

    Added standard header block

---
+++ a03_batemanb.py
@@ -0,0 +1,14 @@
+#################################################################################
+# Author: Brady Bateman
+# Username: batemanb
+#
+# Assignment: A03: Fully Functional Gitty Psychedelic Robotic Turtles
+#
+# Purpose: Continue practicing creating and using functions.
+# Google Doc Link: https://docs.google.com/document/d/1_m_paILsEOJis4kQXogZXEzn9-v-kvkR7mi4Bu0dsP4/edit#
+#
+#################################################################################
+# Acknowledgements: A big thank you to my brother, Dylan Bateman. He helped me figure out the algorithm
+# for drawing the koch snowflake
+#
+#################################################################################
