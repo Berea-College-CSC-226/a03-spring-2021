157530ead1fe9ad9b94440c2c32530bdbb4e3aef
batemanb <batemanb@berea.edu>
2021-02-21 20:45:48 -0500

    added functions to teleport turtles, find the Thue-Morse sequence, create turtles with random colors, and draw a koch snowflake

--- a03_batemanb.py
+++ a03_batemanb.py
@@ -8,8 +8,8 @@
 # Google Doc Link: https://docs.google.com/document/d/1_m_paILsEOJis4kQXogZXEzn9-v-kvkR7mi4Bu0dsP4/edit#
 #
 #################################################################################
-# Acknowledgements: A big thank you to my brother, Dylan Bateman. He helped me figure out the algorithm
-# for drawing the koch snowflake
+# Acknowledgements: A big thank you to my brother, Dylan Bateman. He helped me figure out an algorithm
+# for drawing the koch snowflake in code
 #
 #################################################################################
 
